476ccee4a02ccb9a8e716d9bf51daac70de116a7
Liul2016 <ryan.c.lee1018@gmail.com>
2026-06-01 16:47:05 -0600

    Completed Learning 04 Assignment

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("Ryan", "Inheritance");
+        MathAssignment assignment2 = new MathAssignment("Carter", "Mean Value Theorem", "7.2", "1-8");
+        WritingAssignment assignment3 = new WritingAssignment("Liul", "Culture of Foreign Countries", "The Significance of Ethiopian Culture");
+
+        Console.WriteLine(assignment1.GetSummary());
+        Console.WriteLine();
+
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeworkList());
+        Console.WriteLine();
+
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInformation());
     }
 }
