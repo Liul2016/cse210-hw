6b96db5b74d54c1569bc627c4720ceed076d692e
Liul2016 <ryan.c.lee1018@gmail.com>
2026-06-15 17:59:35 -0600

    Completed W05 Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square sqr = new Square("Red", 5);
+        Rectangle rect = new Rectangle("Green", 3, 5);
+        Circle circle = new Circle("Blue", 4);
+
+        shapes.Add(sqr);
+        shapes.Add(rect);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"---{shape.GetType()}---\nColor: {shape.GetColor()}\nArea: {shape.GetArea()}\n");
+        }
     }
 }
